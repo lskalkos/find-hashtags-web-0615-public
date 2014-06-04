eaab0f5542322f35d5cbb45b69ca0816b509c818
ahimmelstoss <ahimmelstoss@gmail.com>
2014-06-04 15:29:24 -0400

    ready for deploy

--- find_hashtag.rb
+++ find_hashtag.rb
@@ -1,6 +1,3 @@
 def get_hashtags(tweet)
-  tweet.split(" ").collect do |word|
-    hashtag = /^#+[a-zA-Z]+$/.match(word)
-    hashtag ? hashtag.to_s.tr("#", "") : nil
-  end.select { |word| word }
+  #code here
 end
